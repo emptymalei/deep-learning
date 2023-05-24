f6bca4475ed4d2f203bcb5b3f05341bf3fadf2c5
LM <hi@leima.is>
2023-05-24 07:14:08 +0200

    basic trees

---
+++ dl/notebooks/tree_basics.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: deep-learning
+#     language: python
+#     name: python3
+# ---
+
+# +
+import json
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
+from sklearn import tree
