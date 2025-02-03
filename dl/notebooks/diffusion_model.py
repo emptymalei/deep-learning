64feefae093e06e2ee9011d79ae2b41143158b78
LM <hi@leima.is>
2025-02-03 20:57:59 +0100

    add diffusion model notebook

---
+++ dl/notebooks/diffusion_model.py
@@ -0,0 +1,9 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
+# ---
